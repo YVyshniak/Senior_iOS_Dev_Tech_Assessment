7f8527ab9a5c504e04c4fb5c7260bb30c0f7fe3d
YaroslavVysh <yaroslav.vyshniak.dev@gmail.com>
2025-06-05 14:46:17 +0300

    Initial Commit

---
+++ Senior_iOS_Dev_Tech_Assessment/Senior_iOS_Dev_Tech_AssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Senior_iOS_Dev_Tech_AssessmentApp.swift
+//  Senior_iOS_Dev_Tech_Assessment
+//
+//  Created by Yaroslav on 05.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct Senior_iOS_Dev_Tech_AssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
